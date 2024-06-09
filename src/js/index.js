32c0d74325b111112a824af3f8d8b6794762c191
Marquinhos001 <marcos_mendonca001@hotmail.com>
2024-06-09 16:33:31 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById("btn-voltar")
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll('.cartao');
+btnAvancar.addEventListener('click', function () {
+    const ehUltimoCartao = cartaoAtual === cartoes.length - 1
+    if (ehUltimoCartao) return;
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostraCartao();
+
+});
+
+
+btnVoltar.addEventListener('click', function () {
+    const ehPrimeiroCartao =cartaoAtual === 0
+  if (ehPrimeiroCartao)  return;
+  esconderCartao();
+
+  cartaoAtual--;
+  mostrarCartao();
+})
+function esconderCartao() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostraCartao() {
+    cartoes[cartaoAtual].classList.add('selecionado');
+}
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add('selecionado');
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
